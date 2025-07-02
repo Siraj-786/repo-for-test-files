d00b7a346b831bb10e449fc8d6c518c04dbbc1d8
Siraj-786 <sksiraj8466@gmail.com>
2025-07-02 18:32:09 +0530

    Add fdep generation step to CI workflow

---
+++ scripts/generate_fdep_for_test.py
@@ -0,0 +1,8 @@
+from codetraverse.main import create_fdep_data
+
+create_fdep_data(
+    root_dir="sample_code_repos/test_typescript_full",
+    output_base="output/fdep",
+    graph_dir="output/graph",
+    clear_existing=True
+)

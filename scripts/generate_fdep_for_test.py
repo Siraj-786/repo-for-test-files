3b7c54509847e913bda9c8678707f9fe7ab292b5
Siraj-786 <sksiraj8466@gmail.com>
2025-07-02 18:55:23 +0530

    pushing a pass case

--- scripts/generate_fdep_for_test.py
+++ scripts/generate_fdep_for_test.py
@@ -2,7 +2,7 @@ from codetraverse.main import create_fdep_data
 
 create_fdep_data(
     root_dir="sample_code_repos/test_typescript_full",
-    output_base="output/fdep1",
+    output_base="output/fdep",
     graph_dir="output/graph",
     clear_existing=True
 )
